45c6640336f175edb9d826b3f8e5aee0bb648c12
kelvinfi <153182312+kelvinfi@users.noreply.github.com>
2024-02-01 16:18:23 +0000

    Azil config

--- config.js
+++ config.js
@@ -8,14 +8,15 @@ import axios from 'axios'
 
 global.owner = [
   ['233558111386', 'Law(*_*)shambles', true],
+  ['256772742139', 'IsaacAzil', true],
   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['233558111386',]
-global.prems = ['233558111386']
-global.allowed = ['233558111386']
+global.mods = ['233558111386', '256772742139']
+global.prems = ['233558111386', '256772742139']
+global.allowed = ['233558111386', '256772742139']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
